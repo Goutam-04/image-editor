6705a0e3b62d0f3c25d30bc2c34246a614df5e0e
Goutam-04 <goutamnayak2804+github1@gmail.com>
2023-07-30 11:01:25 +0530

    now we can remove the background

--- src/components/Functionality/Functionality.js
+++ src/components/Functionality/Functionality.js
@@ -4,68 +4,104 @@ import { useState } from 'react'
 // API TOKEN: 64c45d2af1b414.18748169
 
 const Functionality = () => {
-
-    const yourToken = '64c45d2af1b414.18748169';
-
-
+    
     const [image, setImage] = useState(null);
     const [bgremove, setBgRemove] = useState(null);
 
-    const uploadImage = () => {
 
-        const fileInput = document.getElementById('fileInput');
-
-
-        var data = new FormData();
-        data.append("image_file", fileInput.files[0], "@path/to/image");
-        // data.append("image_url", "url_to_image");
-
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                console.log(this.responseText);
-            }
-        });
-
-        xhr.open("POST", "https://api.removal.ai/3.0/remove");
-        xhr.setRequestHeader("Rm-Token", yourToken);
-
-        xhr.send(data);
-
-        // const reader = new FileReader();
-
-        // reader.onload = (event) => {
-        //   // The code inside this block will be executed when the file has been successfully loaded.
-        //   setBgRemove = event.target.result;
-        //   // Do something with the fileContent, e.g., display it or process it further.
-        // };
+    const removing =() =>{
+// api key=zjmsBBwViHzzD3P41VMpghS4
         
-        // // Assuming 'file' is the file object obtained from an input file form element.
-        // reader.readAsDataURL(data);
+        const apiKey = "zjmsBBwViHzzD3P41VMpghS4";
+        const url= "https://api.remove.bg/v1.0/removebg "
+        
+        const formData = new FormData()
+        formData.append('image_file',image, image.name)
+        formData.append("size","auto")
 
-
+        
+        fetch(url,{
+            method:'POST',
+            headers:{
+                'X-Api-Key':apiKey,
+            },
+            body:formData
+        }).then((res)=>res.blob()).then((blob)=>{
+            const reader = new FileReader();
+            reader.onloadend = ()=>setBgRemove(reader.result)
+            reader.readAsDataURL(blob);
+        }).catch((error)=>console.log(error))
+        
     }
+        
+    
+    
+    
 
 
 
-    return (
-        <div className='mt-32 mb-32'>
-            <h2 className='text-center font-bold text-lg'>Make your Image Background free👌</h2>
-            <div className='flex justify-center items-center'>
 
-                <input type='file' name='fileInput' id='fileInput' className='mt-8' accept='image/*'
-                    onChange={(e) => setImage(e.target.files[0])}  />
+return (
+    <div className='mt-32 mb-32'>
+        <h2 className='text-center font-bold text-lg'>Make your Image Background free👌</h2>
+        <div className='flex justify-center items-center'>
+        <div>
 
-                <button type='submit' className='mt-8 bg-sky-600 rounded-lg w-24 h-8 font-semibold text-md'
-                    onClick={uploadImage}
-                >Done</button>
-
-                {bgremove && <img src={bgremove} alt="removed background" />}
-            </div>
+            <input type='file' name='fileInput' id='fileInput' className='mt-8' accept='image/*'
+                onChange={(e) => setImage(e.target.files[0])} />
         </div>
-    )
+
+            <button type='submit' className='mt-8 bg-sky-600 rounded-lg w-24 h-8 font-semibold text-md'
+                onClick={ removing}
+            >Done</button>
+
+        </div>
+        <div>
+
+       { bgremove &&<img src={bgremove} alt="removed background" />}
+         
+            
+            
+        </div>
+    </div>
+)
 }
 
 export default Functionality;
+// const yourToken = '64c45d2af1b414.18748169';
+
+// const [image, setImage] = useState(null);
+// const [bgremove, setBgRemove] = useState(null);
+
+// const uploadImage = () => {
+    
+    //     const fileInput = document.getElementById('fileInput');
+    
+    
+    //     var data = new FormData();
+    //     data.append("image_file", fileInput.files[0], "@path/to/image");
+    //     // data.append("image_url", "url_to_image");
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.withCredentials = true;
+
+//     xhr.addEventListener("readystatechange", function () {
+//         if (this.readyState === 4) {
+//             console.log(this.responseText);
+//         }
+//     });
+
+//     xhr.open("POST", "https://api.removal.ai/3.0/remove");
+//     xhr.setRequestHeader("Rm-Token", yourToken);
+
+//     xhr.send(data);
+
+// const reader = new FileReader();
+
+// reader.onload = (event) => {
+//   // The code inside this block will be executed when the file has been successfully loaded.
+//   setBgRemove = event.target.result;
+//   // Do something with the fileContent, e.g., display it or process it further.
+// };
+
+// // Assuming 'file' is the file object obtained from an input file form element.
+// reader.readAsDataURL(data);
